e3bd373b51029fd611cb20c0e021c8708bb4791a
John Tajima <manjiro@gmail.com>
2011-04-07 23:37:16 -0400

    new, simpler implementation using position:fixed layers instead.

--- parallax.js
+++ parallax.js
@@ -3,23 +3,17 @@
  * John Tajima
  * 
  */
-
+ 
 (function($) {
   $.fn.parallax = function(options) { 
     var opts = $.extend({}, $.fn.parallax.defaults, options);
-
-    var windowSize = $(window).height();    // store window size
-    // update window size if when gets resized
-    $(window).resize(function(){
-      windowSize = $(window).height();
-    });
-    
-    // cache frames and add event listener for 'inview'
-    $.fn.parallax.frames = $('.' + opts.frameClass);
-    $($.fn.parallax.frames).bind('inview', frameInViewHandler);
+    var prevPos = 0;  // start pos at 0
 
     // init all data-scrollFactor elements and store orig pos & start/stop and factor
-    $("[data-scrollFactor]").each(function(i,el){
+    var $layers = $("[data-scrollFactor]");
+    
+    // cache some data in each layer
+    $layers.each(function(i,el){
       $(el).data({
         origPos: $(el).position().top,
         scrollFactor: $(el).attr('data-scrollFactor'),
@@ -30,52 +24,41 @@
 
     // listen to scrolling
     $(this).scroll(function(e){
-      var currPos = $(this).scrollTop();
-      var frames = getVisibleFrames(currPos, currPos + windowSize);
-      if (frames.length > 0) { 
-        $.each(frames, function(i,el){
-          $(el).trigger('inview', {'currPos':currPos});
-        });
-      }
-    });
-    
-    // returns frames which are currently visible in browser window
-    function getVisibleFrames(top, bot) {
-      return $.map($.fn.parallax.frames, function(el, i){
-        var frameTop = $(el).position().top;
-        var frameBot = frameTop + $(el).height();
-        return ((frameTop < top && frameBot > bot) || (frameTop < bot && frameBot > top) || (frameTop > top && frameBot < bot)) ? el : null;
-      });
-    }
-
-    // callback when frame is in view
-    // repositions scrollable elements by relPos * factor
-    function frameInViewHandler(e, params) {
-      var $frame   = $(e.target);
-      var frameTop = $frame.position().top;
-      var relPos   = frameTop === 0 ? params.currPos : params.currPos - frameTop;      
-      var currPos  = params.currPos;
+      var currPos = $(this).scrollTop();  // get scroll pos
+      log("curr: "+currPos);
+      var delta = currPos - prevPos;
       
-      $frame.find('[data-scrollFactor]').each(function(i,el){
-        var data = $(el).data();
-        if (relPos < data.scrollStart || relPos > data.scrollEnd) {
-          return;
+      // loop through all layers
+      $layers.each(function(i,el){
+        var params = $(el).data();
+        
+        // calculate newPos for layer        
+        var newPos = params.origPos - currPos;
+        // add/sub back in scroll range
+        if (currPos > params.scrollEnd) {
+          var amt = params.scrollEnd - params.scrollStart;
+        } else if (currPos > params.scrollStart) {
+          var amt = currPos - params.scrollStart;
+        } else {
+          var amt = 0;
         }
-
-        var newPos = data.origPos + (currPos - frameTop) * data.scrollFactor;
-        //$(el).css({'top':newPos + 'px'});
-        $(el).animate({'top':newPos+"px"}, 20);
+        newPos += amt * params.scrollFactor;
+        $(el).css({'top': newPos+"px"}); // move layer        
+        //$(el).animate({'top':newPos+"px"}, 10);
       });
-    };
+
+      prevPos = currPos;
+    });
 
     return this;
   };
 
-  $.fn.parallax.frames = [];
-  $.fn.parallax.defaults = {
-    frameClass: 'frame'
-  };
+  $.fn.parallax.defaults = {};
 
   /* private functions */
-
+  function log(msg) {
+    if (window.debug === true && console && console.log) {
+      console.log(msg);
+    } 
+  }
 })(jQuery);
